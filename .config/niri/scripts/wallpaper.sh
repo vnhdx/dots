536c56527972f002ddda65ea9549fa76c1ecdf5b
Vinh <thanh.vinh@hotmail.com>
2025-07-07 10:00:38 +0700

    niri: bind Mod+Shift+B to change the wallper

--- .config/niri/scripts/wallpaper.sh
+++ .config/niri/scripts/wallpaper.sh
@@ -1,25 +1,37 @@
 #!/usr/bin/bash
 
 unsplash() {
-	local api='https://unsplash.it/1920/1080/?random'
-	curl -sLo ~/.cache/wallpaper.jpg \
-		https://unsplash.it/1920/1080/?random
+  echo 'https://unsplash.it/1920/1080/?random'
 }
 
 bing() {
-	local api='https://bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US'
-	local url=$(curl -s "${api}" | jq -r '.images[0].url')
-	curl -sLo ~/.cache/wallpaper.jpg "${url}"
+  local base_url='https://bing.com'
+  local api="${base_url}/HPImageArchive.aspx?format=js&idx=0&n=1&mkt="
+  local url=$(curl -s "${api}" | jq -r '.images[0].url')
+
+  echo "${base_url}/${url}"
+
+  # local url='https://peapix.com/bing/feed?country='
+  # local api='https://bing.biturl.top?resolution=UHD&format=json&mkt=random'
+  # local url=$(curl -s "${api}" | jq -r '.url')
+  # echo "${url}"
 }
 
 download() {
-	local provier="$1"
-	url=$(unsplash)
-	if declare -f "${provider}" &>/dev/null; then
-		url=$(eval "${provider}")
-		curl -sLo ~/.cache/wallpaper.jpg $(eval "${provider}")
-	fi
+  local provider="$1"
+
+  if declare -f "${provider}" &>/dev/null; then
+    notify-send -u low -t 5000 "${provider}" 'Download the new wallpaper'
+    url=$(eval "${provider}")
+    curl -sLo ~/.cache/wallpaper.jpg $(eval "${provider}")
+  fi
 }
 
-download bing
-# download unsplash
+main() {
+  local provider="${1:-bing}"
+
+  download "${provider}"
+  systemctl --user restart swaybg.service
+}
+
+main
